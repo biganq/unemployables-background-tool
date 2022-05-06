835cbf4b336d75f770163c0f1a7d373e57e10c88
xbox366 <54818355@qq.com>
2022-05-06 12:20:00 +0800

    zombies

---
+++ src/myscript.js
@@ -0,0 +1,5 @@
+        //dom-to-image获取滤镜照片
+        //import domtoimage from 'dom-to-image'
+        document.getElementById("test").onclick = function(){
+            console.log("22222")
+        }
