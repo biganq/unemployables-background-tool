3732b7121facbbc467ead84a3502838c4cc2dccf
xbox366 <54818355@qq.com>
2022-05-06 18:24:31 +0800

    final

--- src/myscript.js
+++
@@ -1,5 +0,0 @@
-        //dom-to-image获取滤镜照片
-        //import domtoimage from 'dom-to-image'
-        document.getElementById("test").onclick = function(){
-            console.log("22222")
-        }
